f306a3bbab5fc6028d17b8e65f95eb1e44d2ab14
owaysisapp <bryanfmarks@gmail.com>
2015-11-11 19:44:13 -0800

    Muted Redis

--- config/sockets.js
+++ config/sockets.js
@@ -44,11 +44,13 @@ module.exports.sockets = {
   // -OR-
   //
 
-  adapter: 'socket.io-redis',
-  host: 'herring.redistogo.com',
-  port: 9601,
-  user: 'redistogo',
-  pass: '46669724e6ff91b7bcc4401bfc07ca5e',
+  // adapter: 'socket.io-redis',
+  // host: 'herring.redistogo.com',
+  // port: 9601,
+  // user: 'redistogo',
+  // pass: '46669724e6ff91b7bcc4401bfc07ca5e',
+
+  
   // host: '127.0.0.1',
   // port: 6379,
   // db: 'sails',
